b45bfa5d979933dd6d1abbaf301c52424248ded1
Sorasit Kateratorn <sorasit.ka@ku.th>
2023-11-08 10:06:23 +0700

    Modify data_processing.py to include code that  converts the new input csv  files into those tables into database

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,6 @@
-import csv, os
+import copy
+import csv
+import os
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -14,6 +16,22 @@ with open(os.path.join(__location__, 'Countries.csv')) as f:
     rows = csv.DictReader(f)
     for r in rows:
         countries.append(dict(r))
+players = []
+with open(os.path.join(__location__, 'Players.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        players.append(dict(r))
+teams = []
+with open(os.path.join(__location__, 'Teams.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        teams.append(dict(r))
+titanic = []
+with open(os.path.join(__location__, 'Titanic.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        titanic.append(dict(r))
+
 
 class DB:
     def __init__(self):
@@ -27,15 +45,16 @@ class DB:
             if table.table_name == table_name:
                 return table
         return None
-    
-import copy
+
+
 class Table:
     def __init__(self, table_name, table):
         self.table_name = table_name
         self.table = table
-    
+
     def join(self, other_table, common_key):
-        joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
+        joined_table = Table(self.table_name + '_joins_' +
+                             other_table.table_name, [])
         for item1 in self.table:
             for item2 in other_table.table:
                 if item1[common_key] == item2[common_key]:
@@ -44,20 +63,20 @@ class Table:
                     dict1.update(dict2)
                     joined_table.table.append(dict1)
         return joined_table
-    
+
     def filter(self, condition):
         filtered_table = Table(self.table_name + '_filtered', [])
         for item1 in self.table:
             if condition(item1):
                 filtered_table.table.append(item1)
         return filtered_table
-    
+
     def aggregate(self, function, aggregation_key):
         temps = []
         for item1 in self.table:
             temps.append(float(item1[aggregation_key]))
         return function(temps)
-    
+
     def select(self, attributes_list):
         temps = []
         for item1 in self.table:
@@ -71,14 +90,21 @@ class Table:
     def __str__(self):
         return self.table_name + ':' + str(self.table)
 
+
 table1 = Table('cities', cities)
 table2 = Table('countries', countries)
+table3 = Table('players', players)
+table4 = Table('teams', teams)
+table5 = Table('titanic', titanic)
 my_DB = DB()
 my_DB.insert(table1)
 my_DB.insert(table2)
+my_DB.inset(table3)
+my_DB.inset(table4)
+my_DB.inset(table5)
 my_table1 = my_DB.search('cities')
 
-print("Test filter: only filtering out cities in Italy") 
+print("Test filter: only filtering out cities in Italy")
 my_table1_filtered = my_table1.filter(lambda x: x['country'] == 'Italy')
 print(my_table1_filtered)
 print()
@@ -102,7 +128,8 @@ print()
 print("Test join: finding cities in non-EU countries whose temperatures are below 5.0")
 my_table2 = my_DB.search('countries')
 my_table3 = my_table1.join(my_table2, 'country')
-my_table3_filtered = my_table3.filter(lambda x: x['EU'] == 'no').filter(lambda x: float(x['temperature']) < 5.0)
+my_table3_filtered = my_table3.filter(lambda x: x['EU'] == 'no').filter(
+    lambda x: float(x['temperature']) < 5.0)
 print(my_table3_filtered.table)
 print()
 print("Selecting just three fields, city, country, and temperature")
@@ -110,14 +137,19 @@ print(my_table3_filtered.select(['city', 'country', 'temperature']))
 print()
 
 print("Print the min and max temperatures for cities in EU that do not have coastlines")
-my_table3_filtered = my_table3.filter(lambda x: x['EU'] == 'yes').filter(lambda x: x['coastline'] == 'no')
-print("Min temp:", my_table3_filtered.aggregate(lambda x: min(x), 'temperature'))
-print("Max temp:", my_table3_filtered.aggregate(lambda x: max(x), 'temperature'))
+my_table3_filtered = my_table3.filter(
+    lambda x: x['EU'] == 'yes').filter(lambda x: x['coastline'] == 'no')
+print("Min temp:", my_table3_filtered.aggregate(
+    lambda x: min(x), 'temperature'))
+print("Max temp:", my_table3_filtered.aggregate(
+    lambda x: max(x), 'temperature'))
 print()
 
 print("Print the min and max latitude for cities in every country")
 for item in my_table2.table:
-    my_table1_filtered = my_table1.filter(lambda x: x['country'] == item['country'])
+    my_table1_filtered = my_table1.filter(
+        lambda x: x['country'] == item['country'])
     if len(my_table1_filtered.table) >= 1:
-        print(item['country'], my_table1_filtered.aggregate(lambda x: min(x), 'latitude'), my_table1_filtered.aggregate(lambda x: max(x), 'latitude'))
+        print(item['country'], my_table1_filtered.aggregate(lambda x: min(
+            x), 'latitude'), my_table1_filtered.aggregate(lambda x: max(x), 'latitude'))
 print()
